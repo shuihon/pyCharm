ebace34deee6f7d532780775aa2751625882825c
shuihon <shuhonrin@gmail.com>
2024-04-15 13:21:51 +0900

    转译符，制表位等换行表示

---
+++ 02Chapter_PythonOutLine/01_change_char.py
@@ -0,0 +1,16 @@
+# @Author   : shuihon
+# @File     : 01_change_char.py
+# @Time     : 2024/4/14 18:36
+print("jacky\t20")
+# \t 表示一个制表符
+print("hello\nhello tom")
+# \n 表示换行符
+# 输出\\
+print("\\")
+#输出单引号‘，
+# \'
+print("\'")
+# \r
+# 表示回车，会使得光标重新定位到该行到首个位置
+print("alice,向左走，\ralice先向前走")
+print("姓名\t\t性别\nalice\tgirl")
