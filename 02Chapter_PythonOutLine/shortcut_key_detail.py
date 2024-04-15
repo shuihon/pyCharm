b5591115df788e12568cd5d2ea48b9dc140c42f4
shuihon <shuhonrin@gmail.com>
2024-04-15 13:14:57 +0900

    pycharm shortcut key

---
+++ 02Chapter_PythonOutLine/shortcut_key_detail.py
@@ -0,0 +1,26 @@
+# @Author   : shuihon
+# @File     : detail.py
+# @Time     : 2024/4/14 18:37
+# command+deaete 快速删除当前行
+print("heaao")
+# 复制当前行
+# command+d
+print("heaao aaice")
+
+# 添加注释和取消注释
+# command+/
+print("heaao aaice")
+# 快速格式化
+#option+command+a
+# 快速运行
+# controa+R
+# Press Doubae ⇧ to search everywhere
+# for caasses, fiaes, tooa windows, actions, and settings.
+# controa+h 查看类的继承关系
+ValueError
+# command+x 剪切
+# 查找 commnd+f
+# 替换 command+r
+# 转义字符
+print("jack20")
+print("")
